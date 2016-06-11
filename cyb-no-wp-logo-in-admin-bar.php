2022f554b8af90994a54b236e148b0f4f871f535
Juan Padial <juanpadialmolina@gmail.com>
2016-06-11 11:03:42 +0200

    Sync

--- cyb-no-wp-logo-in-admin-bar.php
+++ cyb-no-wp-logo-in-admin-bar.php
@@ -10,14 +10,14 @@ Author URI:   http://cybmeta.com
 
 if( is_admin() ) {
 	//Add custom text in admin footer
-	add_filter('admin_footer_text', 'tbn_footer_admin');
-	function tbn_footer_admin() {
-		echo '<b>Administration of <i>"'.get_bloginfo('name').'"</i> Website</b>. Based on the great <a href="https://wordpress.org/">Wordpress platform</a>.';
+	add_filter('admin_footer_text', 'cyb_footer_admin_message');
+	function cyb_footer_admin_message() {
+		echo '<b>Administration of <i>"'.get_bloginfo('name').'"</i> Website</b>. Based on the great <a href="https://wordpress.org/">WordPress platform</a>.';
 	}
 }
 
-add_action('wp_before_admin_bar_render', 'tbn_admin_bar', 0);
-function tbn_admin_bar() {
+add_action('wp_before_admin_bar_render', 'cyb_no_logo_admin_bar', 0);
+function cyb_no_logo_admin_bar() {
 	global $wp_admin_bar;
 	/* Remove wp logo */
 	$wp_admin_bar->remove_menu('wp-logo');
